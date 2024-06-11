06504d1bc60bb4f021e7efdde3f777d08184ee98
Banghao Chi <banghao2@illinois.edu>
2024-06-11 21:11:42 +0800

    Updated print format

--- student_entry.py
+++ student_entry.py
@@ -52,18 +52,18 @@ def get_response(stream = False):
 
                     # 打印最新内容
                     clear_lines()
-                    print("Question:", question, '\n\n')
-                    print("Reference Answer:", ref_answer, '\n\n')
-                    print("Student Answer:", stu_answer, '\n\n')
-                    print("Feedback:", text, '\n\n\n')
+                    print("Question:", question, '\n')
+                    print("Reference Answer:", ref_answer, '\n')
+                    print("Student Answer:", stu_answer, '\n')
+                    print("Feedback:", text, '\n\n')
             example_list[index]["feedback"] = text
             results[index] = example_list[index]
         else:
             text = json.loads(response.text)["text"]
-            print("Question:", question, '\n\n')
-            print("Reference Answer:", ref_answer, '\n\n')
-            print("Student Answer:", stu_answer, '\n\n')
-            print("Feedback:", text, '\n\n\n')
+            print("Question:", question, '\n')
+            print("Reference Answer:", ref_answer, '\n')
+            print("Student Answer:", stu_answer, '\n')
+            print("Feedback:", text, '\n\n')
             example_list[index]["feedback"] = text
             results[index] = example_list[index]
             model_name = json.loads(response.text)["model"]
