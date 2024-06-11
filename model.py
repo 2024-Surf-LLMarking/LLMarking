1940d7b3f315b341114a4311ef7cd7c06596b385
Banghao Chi <banghao2@illinois.edu>
2024-06-11 20:47:02 +0800

    Extracted model directory and name from server code

---
+++ model.py
@@ -0,0 +1,29 @@
+__all__ = ["Qwen/Qwen1.5-14B-Chat-GPTQ-Int4", 
+           "Qwen/Qwen1.5-32B-Chat-AWQ", 
+           "internlm/internlm2-chat-7b", 
+           "01-ai/Yi-1.5-9B-Chat",
+           "modelscope/Yi-1.5-34B-Chat-AWQ",
+           "CohereForAI/aya-23-8B",
+           "meta-llama/Meta-Llama-3-8B-Instruct",
+           "THUDM/glm-4-9b-chat",
+           "Qwen/Qwen2-7B-Instruct",
+           "google/gemma-1.1-7b-it",
+           "mistralai/Mistral-7B-Instruct-v0.3",
+           "microsoft/Phi-3-small-8k-instruct",
+           "openbmb/MiniCPM-2B-dpo-bf16"
+           ]
+
+model_names = ["Qwen1.5-14B",
+               "Qwen1.5-32B",
+               "internlm2-7B",
+               "Yi-1.5-9B",
+               "Yi-1.5-34B",
+               "aya-23-8B",
+               "Llama-3-8B",
+               "ChatGLM4-9B",
+               "Qwen2-7B",
+               "Gemma-1.1-7B",
+               "Mistral-7B-v0.3",
+               "Phi3-small",
+               "MiniCPM-2B"
+               ]
--- vllm_server.py
+++ vllm_server.py
@@ -1,6 +1,7 @@
 from vllm import AsyncEngineArgs,AsyncLLMEngine
 from vllm.inputs import TokensPrompt
 from vllm.sampling_params import SamplingParams
+from model import __all__, model_names
 from transformers import AutoTokenizer, GenerationConfig
 from huggingface_hub import snapshot_download
 from fastapi import FastAPI, Request
@@ -13,37 +14,6 @@ import argparse
 
 # http接口服务
 app=FastAPI()
-
-
-__all__ = ["Qwen/Qwen1.5-14B-Chat-GPTQ-Int4", 
-           "Qwen/Qwen1.5-32B-Chat-AWQ", 
-           "internlm/internlm2-chat-7b", 
-           "01-ai/Yi-1.5-9B-Chat",
-           "modelscope/Yi-1.5-34B-Chat-AWQ",
-           "CohereForAI/aya-23-8B",
-           "meta-llama/Meta-Llama-3-8B-Instruct",
-           "THUDM/glm-4-9b-chat",
-           "Qwen/Qwen2-7B-Instruct",
-           "google/gemma-1.1-7b-it",
-           "mistralai/Mistral-7B-Instruct-v0.3",
-           "microsoft/Phi-3-small-8k-instruct",
-           "openbmb/MiniCPM-2B-dpo-bf16"
-           ]
-
-model_names = ["Qwen1.5-14B",
-               "Qwen1.5-32B",
-               "internlm2-7B",
-               "Yi-1.5-9B",
-               "Yi-1.5-34B",
-               "aya-23-8B",
-               "Llama-3-8B",
-               "ChatGLM4-9B",
-               "Qwen2-7B",
-               "Gemma-1.1-7B",
-               "Mistral-7B-v0.3",
-               "Phi3-small",
-               "MiniCPM-2B"
-               ]
 
 parser = argparse.ArgumentParser()
 
