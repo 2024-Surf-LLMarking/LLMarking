512e3a885eecc255391676d620dff48e51fa965e
Banghao Chi <banghao2@illinois.edu>
2024-07-04 16:20:02 +0800

    Refactored project

--- vllm/vllm_server.py
+++ vllm/vllm_server.py
@@ -2,7 +2,7 @@ from vllm import AsyncEngineArgs,AsyncLLMEngine
 from vllm.inputs import TokensPrompt
 from vllm.sampling_params import SamplingParams
 from model import __all__, model_names
-from prompt_utils import remove_stop_words, _build_prompt_self
+from utils.prompt_utils import remove_stop_words, _build_prompt_self
 from transformers import AutoTokenizer, GenerationConfig
 from huggingface_hub import snapshot_download
 from fastapi import FastAPI, Request
