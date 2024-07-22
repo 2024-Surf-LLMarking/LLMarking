0e654e8e415f6ec801ae0ecfd8fb98cc5aa94094
Banghao <banghao_chi@163.com>
2024-07-22 18:15:00 -0500

    Updated server config

--- vllm_server.py
+++ vllm_server.py
@@ -117,11 +117,11 @@ def load_vllm():
     args.quantization=quantization
     args.gpu_memory_utilization=gpu_memory_utilization
     args.dtype=dtype
-    args.max_num_seqs=10 if model_dir != __all__[22] and model_dir != __all__[23] else 3  # batch最大10条样本
+    args.max_num_seqs=10 if model_dir != __all__[22] and model_dir != __all__[23] else 1  # batch最大10条样本
     if model_dir == __all__[1]:
         args.max_model_len=3024
     elif model_dir == __all__[22] or model_dir == __all__[23]:
-        args.max_model_len=3072
+        args.max_model_len=2800
     else:
         args.max_model_len=generation_config.max_window_size
     # 加载模型
