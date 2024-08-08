be0367fbfa551a793a37f1aea2a313d84c19f138
Banghao <banghao_chi@163.com>
2024-08-08 07:47:25 -0500

    :rocket: Updated CSV output format

--- generate_results_stats.py
+++ generate_results_stats.py
@@ -25,18 +25,41 @@ model_name_list = sorted(model_name_list)
 directory = ["zeroshot", "oneshot", "fewshot"]
 
 eval_results = []
+mismatched_eval_results = []
 for model_name in model_name_list:
     row = [model_name]
+    mismatched_row = [model_name]
     for d in directory:
         print(f"Processing {d} for {model_name}")
         f1, precision, recall, mismatch_percentage = extract_info_from_json(f'results/v{args.prompt}/short/{course}/{d}/{model_name}.json')
+
         if f1 is None:
             cell = f"F1 score:                None\nPrecision score: None\nRecall score:        None\nMismatch percentage: None"
-        else:
+            row.append("None")
+            mismatched_row.append("None")
+        elif mismatch_percentage > 0:
             cell = f"F1 score:                {f1:.4f}\nPrecision score: {precision:.4f}\nRecall score:        {recall:.4f}\nMismatch percentage: {mismatch_percentage:.2f}%"
-        row.append(cell)
-    eval_results.append(row)
+            row.append("Mismatched")
+            mismatched_row.append(cell)
+        else:
+            cell = f"F1 score:                {f1:.4f}\nPrecision score: {precision:.4f}\nRecall score:        {recall:.4f}"
+            row.append(cell)
+            mismatched_row.append("Matched")
+
+    for cell in row[1:]:
+        if cell != "Mismatched" and cell != "None":
+            eval_results.append(row)
+            break
+    for cell in mismatched_row[1:]:
+        if cell != "Matched":
+            mismatched_eval_results.append(mismatched_row)
+            break
+
     with open(f'results/v{args.prompt}/short/{course}/{course}.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerow(["Model Name", "Zero-shot", "One-shot", "Few-shot"])
         writer.writerows(eval_results)
+    with open(f'results/v{args.prompt}/short/{course}/{course}_mismatched.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Model Name", "Zero-shot", "One-shot", "Few-shot"])
+        writer.writerows(mismatched_eval_results)
