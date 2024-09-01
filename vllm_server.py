63e5cf8955fd5361546baa81115979747ed148c3
Banghao <banghao_chi@163.com>
2024-09-01 09:35:10 -0500

    :rocket: Updated GPU detection when using Forward server

--- vllm_server.py
+++ vllm_server.py
@@ -50,7 +50,7 @@ def load_vllm():
     global generation_config, tokenizer, stop_words_ids, engine
     # For cluster
     if arg.model >= 15:
-        os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"
     if arg.model == 28:
         os.environ["VLLM_ATTENTION_BACKEND"] = "FLASHINFER"
     # 模型下载
