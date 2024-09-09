304b5e17cba2c17094bcc19a1337dafff61cc8f1
Banghao <banghao_chi@163.com>
2024-09-09 10:50:14 -0500

    :rocket: Updated gpt4-o-mini results

--- eval_long_gpt.py
+++ eval_long_gpt.py
@@ -1,9 +1,9 @@
-from prompt.prompt_template import dynamic_prompt
-from utils.dynamic_utils import build_diction_from_model
+from prompt.prompt_template_long import prompt_list
+from concurrent.futures import ThreadPoolExecutor
 from utils.count_utils import count_cases
-import concurrent.futures
 from openai import OpenAI
 from tqdm import tqdm
+import threading
 import argparse
 import json
 import csv
@@ -15,22 +15,47 @@ model = "gpt-4o-mini"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--course", "-c", type=str, help="Course name", required=True)
-parser.add_argument("--thread", "-t", type=int, help="Number of threads", default=3)
+parser.add_argument("--thread", "-t", type=int, help="Number of threads", default=10)
+parser.add_argument(
+    "--prompt",
+    "-p",
+    type=int,
+    help="Prompt type to use.",
+    required=True,
+    choices=[3],
+)
 args = parser.parse_args()
 
+directory = ["zeroshot", "oneshot", "fewshot"]
 course = args.course
+model_name = None
+num_thread = args.thread
 
-global data, prompt, example_dict
+global data, question_code_to_example_dict
 data = None
-prompt = None
-example_dict = {}
+question_code_to_example_dict = {}
 
-if not os.path.exists("results/dynamic/long"):
-    print("Creating results/dynamic/long directory...")
-    os.makedirs("results/dynamic/long")
-if not os.path.exists(f"results/dynamic/long/{course}"):
-    print(f"Creating results/dynamic/long/{course} directory...")
-    os.makedirs(f"results/dynamic/long/{course}")
+if not os.path.exists("results"):
+    print("Creating results directory...")
+    os.makedirs("results")
+if not os.path.exists(f"results/v{args.prompt}"):
+    print(f"Creating results/v{args.prompt} directory...")
+    os.makedirs(f"results/v{args.prompt}")
+
+if not os.path.exists(f"results/v{args.prompt}/long"):
+    print(f"Creating results/v{args.prompt}/long directory...")
+    os.makedirs(f"results/v{args.prompt}/long")
+if not os.path.exists(f"results/v{args.prompt}/long/{course}"):
+    print(f"Creating results/v{args.prompt}/long/{course} directory...")
+    os.makedirs(f"results/v{args.prompt}/long/{course}")
+    for d in directory:
+        print(f"Creating results/v{args.prompt}/long/{course}/{d} directory...")
+        os.makedirs(f"results/v{args.prompt}/long/{course}/{d}")
+else:
+    for d in directory:
+        if not os.path.exists(f"results/v{args.prompt}/long/{course}/{d}"):
+            print(f"Creating results/v{args.prompt}/long/{course}/{d} directory...")
+            os.makedirs(f"results/v{args.prompt}/long/{course}/{d}")
 
 with open(f"data/long/{course}/{course}_CSV1.csv", "r") as file:
     csv_reader = csv.reader(file)
@@ -38,19 +63,32 @@ with open(f"data/long/{course}/{course}_CSV1.csv", "r") as file:
     rows = list(csv_reader)
     db_data = {}
     for row in rows:
-        example_text = row[4]
-        parts = re.split(r"\n(?=<Point)", example_text, maxsplit=1)
-        example_stu_answer = parts[0].strip()
-        example_feedback = parts[1].strip() if len(parts) > 1 else ""
-        example_dict[row[0]] = {
-            "example_stu_answer": example_stu_answer,
-            "example_feedback": example_feedback,
-        }
+        example_text_1 = row[5]
+        example_text_2 = row[6]
+        example_text_3 = row[7]
+        parts_1 = re.split(r"\n(?=<Point)", example_text_1, maxsplit=1)
+        parts_2 = re.split(r"\n(?=<Point)", example_text_2, maxsplit=1)
+        parts_3 = re.split(r"\n(?=<Point)", example_text_3, maxsplit=1)
+        example_stu_answer_1 = parts_1[0].strip()
+        example_feedback_1 = parts_1[1].strip() if len(parts_1) > 1 else ""
+        example_stu_answer_2 = parts_2[0].strip()
+        example_feedback_2 = parts_2[1].strip() if len(parts_2) > 1 else ""
+        example_stu_answer_3 = parts_3[0].strip()
+        example_feedback_3 = parts_3[1].strip() if len(parts_3) > 1 else ""
         db_data[row[0]] = {
             "question": row[1],
             "fullMark": row[2],
             "referenceAnswer": row[3],
+            "note": row[4],
             "num_points": count_cases(row[3]),
+        }
+        question_code_to_example_dict[row[0]] = {
+            "example_stu_answer_1": example_stu_answer_1,
+            "example_feedback_1": example_feedback_1,
+            "example_stu_answer_2": example_stu_answer_2,
+            "example_feedback_2": example_feedback_2,
+            "example_stu_answer_3": example_stu_answer_3,
+            "example_feedback_3": example_feedback_3,
         }
 
 with open(f"data/long/{course}/{course}_CSV2.csv", "r") as file:
@@ -66,34 +104,82 @@ with open(f"data/long/{course}/{course}_CSV2.csv", "r") as file:
                 "fullMark": db_data[row[0]]["fullMark"],
                 "num_points": db_data[row[0]]["num_points"],
                 "referenceAnswer": db_data[row[0]]["referenceAnswer"],
+                "note": db_data[row[0]]["note"],
                 "studentAnswer": row[1],
                 "teacherMark": row[2].split("\n")[1].strip('"'),
             }
         )
 
 
-def get_single_response(entry, prompt):
-    global example_dict
-    question = entry["question"]
-    full_mark = entry["fullMark"]
-    ref_answer = entry["referenceAnswer"]
-    stu_answer = entry["studentAnswer"]
-    num_points = entry["num_points"]
-    example_stu_answer = example_dict[entry["question_code"]]["example_stu_answer"]
-    example_feedback = example_dict[entry["question_code"]]["example_feedback"]
-    query = prompt.format(
-        question=question,
-        ref_answer=ref_answer,
-        stu_answer=stu_answer,
-        full_mark=full_mark,
-        num_points=num_points,
-        example_stu_answer=example_stu_answer,
-        example_feedback=example_feedback,
-    )
+def get_response(i, stream=False):
+    global prompt, data, model_name, question_code_to_example_dict
+    results = {}
+    pbar = tqdm(total=len(data), desc=f"Processing {directory[i]}")
 
-    completion = client.chat.completions.create(
-            model=model,
-            messages=[
+    def process_entry(index):
+        global model_name
+        entry = data[index]
+        question = entry["question"]
+        full_mark = entry["fullMark"]
+        ref_answer = entry["referenceAnswer"]
+        stu_answer = entry["studentAnswer"]
+        num_points = entry["num_points"]
+        note = entry["note"]
+        if i == 0:
+            query = prompt.format(
+                question=question,
+                ref_answer=ref_answer,
+                stu_answer=stu_answer,
+                full_mark=full_mark,
+                num_points=num_points,
+                note=note,
+            )
+        elif i == 1:
+            query = prompt.format(
+                question=question,
+                ref_answer=ref_answer,
+                stu_answer=stu_answer,
+                full_mark=full_mark,
+                num_points=num_points,
+                note=note,
+                example_stu_answer_1=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_stu_answer_1"],
+                example_feedback_1=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_feedback_1"],
+            )
+        elif i == 2:
+            query = prompt.format(
+                question=question,
+                ref_answer=ref_answer,
+                stu_answer=stu_answer,
+                full_mark=full_mark,
+                num_points=num_points,
+                note=note,
+                example_stu_answer_1=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_stu_answer_1"],
+                example_feedback_1=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_feedback_1"],
+                example_stu_answer_2=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_stu_answer_2"],
+                example_feedback_2=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_feedback_2"],
+                example_stu_answer_3=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_stu_answer_3"],
+                example_feedback_3=question_code_to_example_dict[
+                    entry["question_code"]
+                ]["example_feedback_3"],
+            )
+
+        completion = client.chat.completions.create(
+        model=model,
+        messages=[
                 {
                     "role": "system",
                     "content": "You are an impartial, objective AI grading assistant.",
@@ -102,75 +188,31 @@ def get_single_response(entry, prompt):
             ],
             temperature=0.0,
         )
-    text = completion.choices[0].message.content
-    return text, model
+        text = completion.choices[0].message.content
+        model_name = model
+        entry["feedback"] = text
+        results[index] = entry
+
+        with lock:
+            pbar.update(1)
+
+    lock = threading.Lock()
+
+    with ThreadPoolExecutor(max_workers=num_thread) as executor:
+        executor.map(process_entry, range(len(data)))
+
+    model_name = model_name if not stream else "streamed_model"
+    index_list, results = zip(*results.items())
+    sorted_results = {
+        index: result
+        for index, result in sorted(zip(index_list, results), key=lambda x: x[0])
+    }
+    with open(
+        f"results/v{args.prompt}/long/{course}/{directory[i]}/{model_name}.json", "w"
+    ) as file:
+        json.dump(sorted_results, file, indent=4)
 
 
-def get_responses(entry, prompt):
-    global example_dict
-    with concurrent.futures.ThreadPoolExecutor(max_workers=args.thread) as executor:
-        futures = [
-            executor.submit(get_single_response, entry, prompt)
-            for _ in range(args.thread)
-        ]
-        results = [
-            future.result() for future in concurrent.futures.as_completed(futures)
-        ]
-
-    for i, (text, model_name) in enumerate(results, 1):
-        entry[f"feedback_{i}"] = text
-
-    has_diff, final_feedback, final_points_dict, mismatch_count, num_of_total_points = (
-        build_diction_from_model(
-            results,
-            entry["num_points"],
-            entry["teacherMark"],
-            args.thread,
-            entry["question"],
-            entry["referenceAnswer"],
-            entry["studentAnswer"],
-        )
-    )
-    if has_diff:
-        example_dict[entry["question_code"]]["example_stu_answer"] = entry[
-            "studentAnswer"
-        ]
-        example_dict[entry["question_code"]]["example_feedback"] = final_feedback
-    entry["model_name"] = model_name
-    entry["feedback"] = final_feedback
-    entry["feedback_dict"] = final_points_dict
-    entry["mismatch_count"] = mismatch_count
-    entry["num_of_total_points"] = num_of_total_points
-    return entry
-
-
-def process_data():
-    global prompt, data
-    results = {}
-
-    def process_entry(index_entry):
-        index, entry = index_entry
-        updated_entry = get_responses(entry.copy(), prompt)
-
-        return index, updated_entry
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=args.thread) as executor:
-        futures = [
-            executor.submit(process_entry, (index, entry))
-            for index, entry in enumerate(data)
-        ]
-
-        for future in tqdm(
-            concurrent.futures.as_completed(futures),
-            total=len(data),
-            desc=f"Processing {course}",
-        ):
-            index, updated_entry = future.result()
-            results[index] = updated_entry
-
-    with open(f"results/dynamic/long/{course}/{model}.json", "w") as file:
-        json.dump(results, file, indent=4)
-
-
-prompt = dynamic_prompt
-process_data()
+for i in range(len(directory)):
+    prompt = prompt_list[i]
+    get_response(i, False)
